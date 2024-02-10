eb6b3ff9a4a56547e143e8cecc09f68e94fb8e22
garrethil <garrethildebrandt@gmail.com>
2024-02-10 13:54:35 -0500

    complete all GET routes in server.js

--- server.js
+++ server.js
@@ -1,15 +1,42 @@
 const express = require('express');
+const fs = require('fs');
 const path = require('path');
-
+const PORT = 3001;
 const app = express();
 
 app.use(express.static('public'));
 
 
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, 'notes.html'));
+    res.sendFile(path.join(__dirname, 'public', 'notes.html'));
 });
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+    const filePath = path.join(__dirname, 'db', 'db.json');
+
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if(err){
+            console.error(err);
+            res.status(500).send('Error reading data file');
+            return;
+        }
+
+        try {
+            res.json(JSON.parse(data));
+        } catch (error) {
+            console.error(error);
+            res.status(500).send('error parsing data');
+        }
+    })
+})
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`listening at http://localhost:${PORT}`);
 });
